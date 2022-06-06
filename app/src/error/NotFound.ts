6e13c476c6b9e4a531b92bfde33681450dcd5b00
ercumentlacin <ercument.lacin@akinon.com>
2022-06-06 21:58:47 +0300

    remove log

--- app/src/error/NotFound.ts
+++ app/src/error/NotFound.ts
@@ -4,7 +4,6 @@ import { HttpException } from './HttpException';
 
 export class NotFound extends HttpException {
   constructor(message: string) {
-    console.log(httpStatus.NOT_FOUND);
     super(httpStatus.NOT_FOUND, message);
   }
 }
